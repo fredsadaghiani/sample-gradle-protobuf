d8d7a03fc1099744f0d962449c3cc242b8b27a6a
Fred Sadaghiani <fred.sadaghiani@gmail.com>
2016-06-08 20:31:38 -0700

    Upgrade code to use latest versions

--- src/main/java/com/vach/tryout/protobuf/Main.java
+++ src/main/java/com/vach/tryout/protobuf/Main.java
@@ -1,8 +1,15 @@
 package com.vach.tryout.protobuf;
 
+import com.vach.tryout.AddressBookProtos.Person;
+
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("something");
-  }
+    public static void main(String[] args) {
+        System.out.println("something");
+
+        Person.Builder p = Person.newBuilder();
+        p.setEmail("email");
+        p.build();
+
+    }
 }
